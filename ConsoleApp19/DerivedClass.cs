4231a8eb7e50b57d70b66b30c1aa2bf1b35e0759
exuteboll <exuteboll4@gmail.com>
2025-04-17 10:57:55 +0300

    Add files via upload

---
+++ ConsoleApp19/DerivedClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class DerivedClass : IInterface3
+    {
+        public void Method1()
+        {
+            Console.WriteLine($" ета перший метод");
+        }
+        public void Method2()
+        {
+            Console.WriteLine($" другий метод");
+        }
+        public void Method3()
+        {
+            Console.WriteLine($" Третий метод");
+        }
+    }
+}
---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dClass = new DerivedClass();
+            dClass.Method1();
+            dClass.Method2();
+            dClass.Method3();
+            Console.ReadLine();
+        }
+    }
+}
