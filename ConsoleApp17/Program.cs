52418c158e8f362c2aeb74813d23f564dcd456a6
exuteboll <exuteboll4@gmail.com>
2025-04-17 10:21:54 +0300

    Add files via upload

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dClass = new DerivedClass();
+            dClass.Method();
+            Console.ReadLine();
+        }
+    }
+}
